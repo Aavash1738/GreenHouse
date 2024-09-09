3afba2214efc0802cb3d1407cba38c5d81405c03
Aavash1738 <bhattaraiaavash1@gmail.com>
2024-09-09 21:04:52 +0545

    Before routing setup push 3

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="bg-dark">
+        <h1 className="text-success">Yes</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;

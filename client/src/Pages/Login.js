54fe6ac1c02deec49217c59488b23be95858f3ba
Aavash1738 <bhattaraiaavash1@gmail.com>
2024-09-10 19:10:07 +0545

    User login and registration validation complete push 7

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,11 +1,26 @@
 import React from "react";
-import { Form, Input } from "antd";
+import { Form, Input, message } from "antd";
 import "../styles/LoginStyles.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
-  const onfinishHandler = (values) => {
-    console.log(values);
+  const navigate = useNavigate();
+
+  const onfinishHandler = async (values) => {
+    try {
+      const res = await axios.post("/api/v1/user/login", values);
+      if (res.data.success) {
+        localStorage.setItem("token", res.data.token);
+        message.success("Login Successfuly");
+        navigate("/");
+      } else {
+        message.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      message.error("Something went wrong");
+    }
   };
 
   return (
--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -1,11 +1,24 @@
 import React from "react";
-import { Form, Input } from "antd";
+import { Form, Input, message } from "antd";
 import "../styles/RegisterStyles.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
-  const onfinishHandler = (values) => {
-    console.log(values);
+  const navigate = useNavigate();
+  const onfinishHandler = async (values) => {
+    try {
+      const res = await axios.post("/api/v1/user/register", values);
+      if (res.data.success) {
+        message.success("Registered successfully");
+        navigate("/login");
+      } else {
+        message.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      message.error("Something went wrong");
+    }
   };
 
   return (
